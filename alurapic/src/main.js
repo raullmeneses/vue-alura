52864101d86b38926ab1d78e1ef6b9796c64af2d
João Raul <raullmeneses@hotmail.com>
2018-06-05 16:36:06 -0300

    Finalizada aula 5 do curso de Vue.js II

--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -3,16 +3,27 @@ import App from './App.vue'
 import VueResouce from 'vue-resource';
 import VueRouter from 'vue-router';
 import { routes } from './routes';
+import VeeValidate from 'vee-validate';
+import dicionarioPtBR from './pt_BR';
 
 Vue.use(VueResouce);
 Vue.http.options.root = 'http://localhost:3000';
 
 Vue.use(VueRouter);
-
 const router = new VueRouter({ 
   routes,
   mode: 'history' // isso aqui é para nao aparecer o # antes dos links da rota
 });
+
+Vue.use(VeeValidate, {
+  
+  locale: 'pt_BR',
+  dictionary: {
+    pt_BR: dicionarioPtBR
+  }
+
+});
+
 
 new Vue({
   el: '#app',
